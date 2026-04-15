dec3c598f94c2a6c59394e6c96d46301f4fffb15
Mahfudz Alfanani Syaviqi <mahfudzworksss@gmail.com>
2026-04-15 05:57:47 -0700

    fix base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/Project_Weather-APP/", // ⬅️ WAJIB
   plugins: [tailwindcss(), react()],
 });
